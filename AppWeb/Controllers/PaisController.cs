1ec0fc1e92125707d82c555daad777486dfa99a8
Carlos Barbieri <cbarbieri@mobilitysol.com>
2024-03-15 12:18:41 -0300

    feat:Se implementa mvc and domain for abm bibiotecas

---
+++ AppWeb/Controllers/PaisController.cs
@@ -0,0 +1,156 @@
+﻿using Dominio.Entidades;
+using Dominio.Excepciones.Pais;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppWeb.Controllers
+{
+	public class PaisController : Controller
+	{
+		private static List<Pais> _pais = new List<Pais>();
+
+		// GET: PaisController
+		public IActionResult Index(string mensaje)
+		{
+			ViewBag.Mensaje = mensaje;
+			ViewBag.ExitoMessage = TempData["ExitoMessage"];
+			ViewBag.ListaPaises = _pais;
+			return View(_pais);
+		}
+
+		// GET: PaisController/Details/5
+		public IActionResult Details(int id)
+		{
+			var pais = _pais.FirstOrDefault(p => p.Id == id);
+			if (pais == null)
+			{
+				return NotFound();
+			}
+			return View(pais);
+		}
+
+		// GET: PaisController/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: PaisController/Create
+		[HttpPost]
+		public IActionResult Create(Pais pais)
+		{
+			if (pais == null)
+			{
+				return NotFound();
+			}
+
+			try
+			{
+				pais.Validar();
+				_pais.Add(pais);
+				TempData["ExitoMessage"] = "Se dio de alta un país";
+				return RedirectToAction("Index");
+			}
+			catch (IdInvalidaException ex)
+			{
+				ViewBag.Error = ex.Message;
+			}
+			catch (NombreInvalidaException ex)
+			{
+				ViewBag.Error = ex.Message;
+			}
+			catch (CantHabitantesInvalidaException ex)
+			{
+				ViewBag.Error = ex.Message;
+			}
+			catch (ArgumentException ex)
+			{
+				ViewBag.Error = ex.Message;
+			}
+			catch (Exception ex)
+			{
+				ViewBag.Error = "Hubo un problema. Intenta nuevamente o llame a soporte";
+			}
+			ViewBag.ListaPaises = _pais;
+			return View(pais);
+		}
+
+		// GET: PaisController/Edit/5
+		public IActionResult Edit(Pais unPais)
+		{
+			var pais = _pais.FirstOrDefault(p => p.Id == unPais.Id);
+			ViewBag.ListaPaises = _pais;
+			return View(pais);
+		}
+
+		// POST: PaisController/Edit/5
+		[HttpPost]
+		public IActionResult Edit(int id, Pais pais)
+		{
+			try
+			{
+				if (!ModelState.IsValid)
+				{
+					ViewBag.Error = "La información ingresada no es válida.";
+					return View(pais);
+				}
+
+				var existingPais = _pais.FirstOrDefault(p => p.Id == id);
+				if (existingPais == null)
+				{
+					return NotFound();
+				}
+
+				existingPais.Nombre = pais.Nombre;
+				existingPais.CantHabitantes = pais.CantHabitantes;
+
+				TempData["ExitoMessage"] = "Se actualizó el país";
+				return RedirectToAction("Index");
+			}
+			catch (Exception ex)
+			{
+				ViewBag.Error = "Hubo un problema. Intenta nuevamente o llama a soporte: " + ex.Message;
+			}
+
+			ViewBag.ListaPaises = _pais;
+			return View(pais);
+		}
+
+		// GET: PaisController/Delete/5
+		[HttpGet]
+		public IActionResult Delete(int id)
+		{
+			var paisToDelete = _pais.FirstOrDefault(p => p.Id == id);
+			if (paisToDelete == null)
+			{
+				return NotFound();
+			}
+
+			return View(paisToDelete);
+		}
+
+		// POST: PaisController/Delete/5
+		[HttpPost]
+		public IActionResult Delete(Pais pais)
+		{
+			try
+			{
+				// Find the pais to delete
+				var paisToDelete = _pais.FirstOrDefault(p => p.Id == pais.Id);
+				if (paisToDelete == null)
+				{
+					throw new Exception("No se encontró el país a eliminar");
+				}
+
+				// Delete the data and redirect to the index page
+				_pais.RemoveAll(p => p.Id == pais.Id);
+				TempData["ExitoMessage"] = "Se eliminó el país";
+				return RedirectToAction("Index");
+			}
+			catch (Exception ex)
+			{
+				ViewBag.Error = ex.Message;
+				return RedirectToAction("Index");
+			}
+		}
+	}
+}
---
+++ AppWeb/Controllers/TemaController.cs
@@ -0,0 +1,152 @@
+﻿using Dominio.Entidades;
+using Dominio.Excepciones.Tema;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppWeb.Controllers
+{
+	public class TemaController : Controller
+	{
+		private static List<Tema> _tema = new List<Tema>();
+
+		// GET: TemaController
+		public IActionResult Index(string mensaje)
+		{
+			ViewBag.Mensaje = mensaje;
+			ViewBag.ExitoMessage = TempData["ExitoMessage"];
+			ViewBag.ListaPaises = _tema;
+			return View(_tema);
+		}
+
+		// GET: TemaController/Details/5
+		public IActionResult Details(int id)
+		{
+			var tema = _tema.FirstOrDefault(t => t.Id == id);
+			if (tema == null)
+			{
+				return NotFound();
+			}
+			return View(tema);
+		}
+
+		// GET: TemaController/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: TemaController/Create
+		[HttpPost]
+		public IActionResult Create(Tema tema)
+		{
+			if (tema == null)
+			{
+				return NotFound();
+			}
+
+			try
+			{
+				tema.Validar();
+				_tema.Add(tema);
+				TempData["ExitoMessage"] = "Se dio de alta un tema";
+				return RedirectToAction("Index");
+			}
+			catch (IdInvalidaException ex)
+			{
+				ViewBag.Error = ex.Message;
+			}
+			catch (DescriptionInvalidaException ex)
+			{
+				ViewBag.Error = ex.Message;
+			}
+			catch (ArgumentException ex)
+			{
+				ViewBag.Error = ex.Message;
+			}
+			catch (Exception ex)
+			{
+				ViewBag.Error = "Hubo un problema. Intenta nuevamente o llame a soporte";
+			}
+			ViewBag.ListaTemas = _tema;
+			return View(tema);
+		}
+
+		// GET: TemaController/Edit/5
+		public IActionResult Edit(Tema unTema)
+		{
+			var tema = _tema.FirstOrDefault(p => p.Id == unTema.Id);
+			ViewBag.ListaTemas = _tema;
+			return View(tema);
+		}
+
+		// POST: TemController/Edit/5
+		[HttpPost]
+		public IActionResult Edit(int id, Tema tema)
+		{
+			try
+			{
+				if (!ModelState.IsValid)
+				{
+					ViewBag.Error = "La información ingresada no es válida.";
+					return View(tema);
+				}
+
+				var existingTema = _tema.FirstOrDefault(t => t.Id == id);
+				if (existingTema == null)
+				{
+					return NotFound();
+				}
+
+				existingTema.Nombre = tema.Nombre;
+				existingTema.Description = tema.Description;
+
+				TempData["ExitoMessage"] = "Se actualizó el tema";
+				return RedirectToAction("Index");
+			}
+			catch (Exception ex)
+			{
+				ViewBag.Error = "Hubo un problema. Intenta nuevamente o llama a soporte: " + ex.Message;
+			}
+
+			ViewBag.ListaTemas = _tema;
+			return View(tema);
+		}
+
+		// GET: TemaController/Delete/5
+		[HttpGet]
+		public IActionResult Delete(int id)
+		{
+			var temaToDelete = _tema.FirstOrDefault(p => p.Id == id);
+			if (temaToDelete == null)
+			{
+				return NotFound();
+			}
+
+			return View(temaToDelete);
+		}
+
+		// POST: TemaController/Delete/5
+		[HttpPost]
+		public IActionResult Delete(Tema tema)
+		{
+			try
+			{
+				// Find the pais to delete
+				var temaToDelete = _tema.FirstOrDefault(p => p.Id == tema.Id);
+				if (temaToDelete == null)
+				{
+					throw new Exception("No se encontró el tema a eliminar");
+				}
+
+				// Delete the data and redirect to the index page
+				_tema.RemoveAll(p => p.Id == tema.Id);
+				TempData["ExitoMessage"] = "Se eliminó el tema";
+				return RedirectToAction("Index");
+			}
+			catch (Exception ex)
+			{
+				ViewBag.Error = ex.Message;
+				return RedirectToAction("Index");
+			}
+		}
+	}
+}
